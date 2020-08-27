531e905c1a29eb6f8306bd2e4d4cd5a7b143ef2a
petarcorluka86 <petarcorluka86@gmail.com>
2020-08-27 16:21:15 +0200

    added strict mode

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 
 ReactDOM.render(
-  <Routes />,
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
   document.getElementById('root')
 );
